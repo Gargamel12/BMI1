3fdae91966d6dc97d4c501269200e0c086438b1c
Gargamel12 <Janecek.Ales@stud.vassboskovice.cz>
2021-10-25 19:51:41 +0200

    >< opraveny

--- BMI1/Program.cs
+++ BMI1/Program.cs
@@ -15,20 +15,19 @@ namespace BMI
             Výška = double.Parse(Console.ReadLine());
             var BMI = Hmotnost / (Výška * Výška);
             Console.WriteLine($"Vaše BMI je {BMI} ");
-            Console.ReadLine();
-            if (BMI <= 1! & BMI >=  20) 
+            if (BMI > 1! & BMI < 20) 
             {
                 Console.WriteLine("Trošku hubenější jakože");
             }
-            if (BMI <= 21! & BMI >= 25) 
+            if (BMI > 21! & BMI < 25) 
             {
                 Console.WriteLine("Ideálka kámo");
             }
-            if (BMI <= 26! & BMI >= 30) 
+            if (BMI > 26! & BMI < 30) 
             {
                 Console.WriteLine("Měl by jsi začít hubnout");
             }
-            if (BMI >= 31) 
+            if (BMI > 31)
             {
                 Console.WriteLine("Tlustší jak Homer Simpson");
             }
