6e11d953501ec64991c35542fe56a976a19c5c28
Gargamel12 <Janecek.Ales@stud.vassboskovice.cz>
2021-10-25 18:15:03 +0200

    Add project files.

---
+++ BMI1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Hmotnost;
+            Double Výška;
+            Console.WriteLine("Výpočet BMI");
+            Console.WriteLine("==========");
+            Console.Write("Zadej váhu(kg):");
+            Hmotnost = double.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej výšku(m):");
+            Výška = double.Parse(Console.ReadLine());
+            var BMI = Hmotnost / (Výška * Výška);
+            Console.WriteLine($"Vaše BMI je {BMI} ");
+            Console.ReadLine();
+
+
+        }
+    }
+}
