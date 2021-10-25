aac80c6252b5fe0834e1bed98e75cd6c77fb52ce
Gargamel12 <Janecek.Ales@stud.vassboskovice.cz>
2021-10-25 19:43:08 +0200

    Problém

--- BMI1/Program.cs
+++ BMI1/Program.cs
@@ -16,8 +16,22 @@ namespace BMI
             var BMI = Hmotnost / (Výška * Výška);
             Console.WriteLine($"Vaše BMI je {BMI} ");
             Console.ReadLine();
-
-
+            if (BMI <= 1! & BMI >=  20) 
+            {
+                Console.WriteLine("Trošku hubenější jakože");
+            }
+            if (BMI <= 21! & BMI >= 25) 
+            {
+                Console.WriteLine("Ideálka kámo");
+            }
+            if (BMI <= 26! & BMI >= 30) 
+            {
+                Console.WriteLine("Měl by jsi začít hubnout");
+            }
+            if (BMI >= 31) 
+            {
+                Console.WriteLine("Tlustší jak Homer Simpson");
+            }
         }
     }
 }
